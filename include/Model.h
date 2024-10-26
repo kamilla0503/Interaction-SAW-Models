56de515102f064823fceb3b81b0f8395528e9d6f
kamilla0503 <kamilla.faizullina@yandex.ru>
2024-10-26 17:46:44 +0300

    add inline words

--- include/Model.h
+++ include/Model.h
@@ -41,10 +41,10 @@ public:
     SAW_model() {};
     SAW_model(long length);
 
-    void Reconnect(short direction); //Only Geometry changes --- the same for all SAW Models
+    KOKKOS_INLINE_FUNCTIONvoid Reconnect(short direction); //Only Geometry changes --- the same for all SAW Models
 
-    virtual void FlipMove_AddEnd (long direction, SpinType spinvalue) = 0; //depends on spin variables
-    virtual void FlipMove_AddStart (long direction, SpinType spinvalue) = 0; //depends on spin variables
+    KOKKOS_INLINE_FUNCTION virtual void FlipMove_AddEnd (long direction, SpinType spinvalue) = 0; //depends on spin variables
+    KOKKOS_INLINE_FUNCTION virtual void FlipMove_AddStart (long direction, SpinType spinvalue) = 0; //depends on spin variables
    // virtual void ClusterStep (double flipdirection) = 0; //depends on spin variables
 
     void LatticeInitialization();
