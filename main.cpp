60b90a0c00de1c57a951edfe78994d7693182697
kamilla0503 <kamilla.faizullina@yandex.ru>
2024-11-04 23:33:48 +0300

    some updates

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
 
 
     Kokkos::initialize(Kokkos::InitializationSettings()
-                               .set_disable_warnings(false).set_debug_mode(true)
+                             //  .set_disable_warnings(false).set_debug_mode(true)
 //.set_num_threads(1)
                                );
 
