d5e7cf6cc4fcfc0ee05a5ab3bc87eac00a4a438e
kamilla0503 <kamilla.faizullina@yandex.ru>
2024-11-06 00:22:22 +0300

    some updates

--- Model.cpp
+++ Model.cpp
@@ -184,7 +184,7 @@ void XY_SAW_LongInteraction::StartConfiguration() {
     Kokkos::deep_copy(next_monomers, h_next_monomers_h);
     Kokkos::deep_copy(directions, h_directions_h);
 
-    long lattice_side_host = lattice->lattice_side ; // Assign the actual value you need here
+    lattice_side_host = lattice->lattice_side ; // Assign the actual value you need here
     //lattice_side = Kokkos::View<long, Kokkos::CudaSpace>("lattice_side");
     //Kokkos::deep_copy(lattice_side, lattice_side_host);
     //lattice_side = Kokkos::create_mirror_view_and_copy(Kokkos::CudaHostPinnedSpace(),lattice_side_host);
