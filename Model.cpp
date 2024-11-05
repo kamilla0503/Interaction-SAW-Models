e49eeddd8e063d048f6cc8e05976445158ab0b51
kamilla0503 <kamilla.faizullina@yandex.ru>
2024-11-05 23:12:38 +0300

    some updates

--- Model.cpp
+++ Model.cpp
@@ -202,7 +202,7 @@ double XY_SAW_LongInteraction::Energy() {
         printf(" i = %ld \n", i);
         double energy_i = 0.0;  // Local energy contribution for this i
         if (i < L ) {
-            printf(" i = %ld \n", i);
+            printf(" ei = %ld \n", i);
         // Inner loop remains sequential for each i
             for (long j = i + 1; j < L; j++) {
             //printf()
@@ -215,7 +215,7 @@ double XY_SAW_LongInteraction::Energy() {
             }
         }
         local_H += energy_i;  // Add local energy contribution to the reduction variable
-        printf(" i = %ld %f \n", i, energy_i);
+        printf(" energy i = %ld %f \n", i, energy_i);
     }, H);  // H is the total energy accumulated across all threads
     //Kokkos::fence();
     return -H;  // Return negative of the total energy
