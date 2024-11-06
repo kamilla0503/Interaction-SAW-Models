36743aee296b7feec14310e2b0f141f120e44dac
kamilla0503 <kamilla.faizullina@yandex.ru>
2024-11-06 14:15:19 +0300

    some updates

--- Model.cpp
+++ Model.cpp
@@ -252,6 +252,8 @@ double XY_SAW_LongInteraction::Energy() {
         // Inner loop remains sequential for each i
             for (long j = i + 1; j < local_L; j++) {
             //printf()
+                r = radius( 1, 10,
+                           lattice_side_local);
                 printf(" ej = %ld %ld %ld \n", i, j, lattice_side_local);
                 r = radius(lattice_nodes_positions(i), lattice_nodes_positions(j),
                            lattice_side_local);
