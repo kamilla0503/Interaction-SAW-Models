aabf7e9902beb138fbdbb1fda89fae2aa857f7f1
kamilla0503 <kamilla.faizullina@yandex.ru>
2024-11-07 21:02:02 +0300

    some updates

--- Model.cpp
+++ Model.cpp
@@ -227,6 +227,7 @@ void XY_SAW_LongInteraction::StartConfiguration() {
     printf("Finish E= %f \n", E);
 
 // Random pool
+    //  rand_pool = Kokkos::Random_XorShift64_Pool<Kokkos::Cuda>(/* seed or execution space */);
    // flip_data.rand_pool = rand_pool;
 
 }
