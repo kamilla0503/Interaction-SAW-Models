b1975d420081e3df75371631e33634ab18d1655e
kamilla0503 <kamilla.faizullina@yandex.ru>
2024-11-09 00:47:10 +0300

    some updates

--- Model.cpp
+++ Model.cpp
@@ -300,7 +300,12 @@ std::mt19937 generator(std::chrono::steady_clock::now().time_since_epoch().count
 KOKKOS_INLINE_FUNCTION
 void XY_SAW_LongInteraction::FlipMove_AddEnd(long direction, double spinValue) {
     Kokkos::parallel_for("FlipMove_AddEnd", 1, KOKKOS_LAMBDA(const int idx) {
-
+        printf("XY_SAW_LongInteraction:: FlipMove_AddEnd   direction =  %d  \n",
+              direction
+        );
+        printf("XY_SAW_LongInteraction:: FlipMove_AddEnd  end = %d ; direction =  %d  \n",
+                flip_data.end_conformation, direction
+        );
         printf("XY_SAW_LongInteraction:: FlipMove_AddEnd ndim2 = %d; end = %d ; direction =  %d  \n",
                flip_data.ndim2, flip_data.end_conformation, direction
                );
