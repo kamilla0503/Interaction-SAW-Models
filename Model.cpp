e293dd72fffbac6ee9925f7509e8ff75b9a12027
kamilla0503 <kamilla.faizullina@yandex.ru>
2024-11-10 22:42:23 +0300

    some updates

--- Model.cpp
+++ Model.cpp
@@ -553,8 +553,32 @@ void XY_SAW_LongInteraction::Reconnect(short direction) {
 }
 
 void XY_SAW_LongInteraction::updateData() {
+    // Create a host mirror of flip_data.E
+    auto start_host = Kokkos::create_mirror_view(flip_data.start_conformation);
+// Copy data from device to host
+    Kokkos::deep_copy(start_host, flip_data.start_conformation);
+// Access the value on the host
+    start_conformation = start_host(0);
+
+    auto end_host = Kokkos::create_mirror_view(flip_data.end_conformation);
+// Copy data from device to host
+    Kokkos::deep_copy(end_host, flip_data.end_conformation);
+// Access the value on the host
+    end_conformation = end_host(0);
+
+
     double r2 = lattice->radius(start_conformation, end_conformation);
     e2e_distance_2 << r2;
+
+
+
+// Create a host mirror of flip_data.E
+    auto E_host = Kokkos::create_mirror_view(flip_data.E);
+// Copy data from device to host
+    Kokkos::deep_copy(E_host, flip_data.E);
+// Access the value on the host
+    E = E_host(0);
+
     energy << E;
     energy_2 << E * E;
     energy_4 << E * E * E * E;
@@ -562,6 +586,9 @@ void XY_SAW_LongInteraction::updateData() {
     double sum_sin_1 = 0.0;
     double sum_cos_1 = 0.0;
     long int current = start_conformation;
+
+    Kokkos::deep_copy(h_sequence_on_lattice_h, flip_data.sequence_on_lattice);
+
     for (int e = 0; e < L; e++) {
         sum_sin_1 += sin(h_sequence_on_lattice_h[current]);
         sum_cos_1 += cos(h_sequence_on_lattice_h[current]);
