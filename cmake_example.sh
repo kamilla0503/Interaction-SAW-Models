3e0a7d9f443d6537733e918cfe87cb62adbbf87b
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-01-23 22:34:04 +0300

    Try AddEnd in One Kernel

--- cmake_example.sh
+++ cmake_example.sh
@@ -1,2 +1,2 @@
 cmake .. -DKokkos_DIR="${HOME}/kokkos/install/lib/cmake/Kokkos" -DCMAKE_BUILD_TYPE=Release
-
+cmake .. -DCMAKE_BUILD_TYPE=Release          -DKokkos_ENABLE_CUDA=ON          -DKokkos_ARCH_AMPERE80=ON          -DKokkos_ARCH_VOLTA70=OFF          -DKokkos_ENABLE_SERIAL=OFF          -DKokkos_ENABLE_OPENMP=OFF
