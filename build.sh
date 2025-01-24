5fd6825e3da910f3751f3fa4cc805cea3d7c75c8
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-01-24 23:02:24 +0300

    Try AddEnd in One Kernel

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 module load CUDA
 module load cmake/3.21.3
 rm -rf build; mkdir build; cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release          -DKokkos_ENABLE_CUDA=ON   -DKokkos_ARCH_VOLTA70=ON    -DKokkos_ENABLE_SERIAL=ON
-make 
+#cmake .. -DCMAKE_BUILD_TYPE=Release          -DKokkos_ENABLE_CUDA=ON   -DKokkos_ARCH_VOLTA70=ON    -DKokkos_ENABLE_SERIAL=ON
+cmake .. -DCMAKE_BUILD_TYPE=Release -DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_VOLTA70=ON -DKokkos_ENABLE_SERIAL=ON -DKokkos_FORCE_DEVICE=ON -DKokkos_ENABLE_HWLOC=OFF
+make
