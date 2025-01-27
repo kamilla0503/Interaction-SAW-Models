97095460c898310eb12fc97985845148a79b3de5
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-01-27 21:31:30 +0300

    errors in energy

--- Model.cpp
+++ Model.cpp
@@ -639,7 +639,7 @@ void hierarchicalEnergy(const Kokkos::TeamPolicy<Kokkos::Cuda>::member_type &tea
                 },
                 energy_i
         );
-        Kokkos::single(Kokkos::PerTeam(team), [&]() {
+        Kokkos::single(Kokkos::PerTeam(team_member), [&]() {
             H_total -= energy_i;
         });
     },
