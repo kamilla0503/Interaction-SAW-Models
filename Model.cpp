5e518b1a077bb782c9e721ca07cdb5a097c30280
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-01-27 21:43:04 +0300

    errors in energy

--- Model.cpp
+++ Model.cpp
@@ -610,6 +610,7 @@ void hierarchicalEnergy(const Kokkos::TeamPolicy<Kokkos::Cuda>::member_type &tea
         printf("finish hierarchicalEnergy %f \n", flip_data.newE());
         printf("flip_data.lattice_side_device = %d \n", flip_data.lattice_side_device);
     }); */
+    double totalEnergy = 0.0;
 
     Kokkos::parallel_reduce(
             Kokkos::TeamThreadRange(team_member, flip_data.L),
@@ -643,8 +644,12 @@ void hierarchicalEnergy(const Kokkos::TeamPolicy<Kokkos::Cuda>::member_type &tea
             H_total -= energy_i;
        // });
     },
-    flip_data.newE
+            Kokkos::Sum<double>(totalEnergy)
     );
+
+    Kokkos::single(Kokkos::PerTeam(team), [&]() {
+        flip_data.newE() = totalEnergy;
+    });
 }
 
 KOKKOS_INLINE_FUNCTION
