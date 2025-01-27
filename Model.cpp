f3d9c5bffa95f74e48046fcad1728d4773626ae8
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-01-27 18:18:54 +0300

    errors in energy

--- Model.cpp
+++ Model.cpp
@@ -771,7 +771,7 @@ void XY_SAW_LongInteraction::FlipMove_AddEnd() {
     static Kokkos::Random_XorShift64_Pool<Kokkos::Cuda> my_pool;
     if (!pool_initialized) {
          //my_pool = Kokkos::Random_XorShift64_Pool<Kokkos::Cuda>(Kokkos::Cuda(), 256, 12345); // seed
-        my_pool.init(Kokkos::Cuda(), 256, 12345);
+        my_pool.init(256, 12345);
         pool_initialized = true;
     }
     auto local_pool = my_pool;
