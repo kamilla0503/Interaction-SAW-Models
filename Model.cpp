339f9d9d808b65fb3e1c5a862c6b1185f8bad266
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-01-28 21:43:57 +0300

    errors in energy

--- Model.cpp
+++ Model.cpp
@@ -620,7 +620,7 @@ void hierarchicalEnergy(const Kokkos::TeamPolicy<Kokkos::Cuda>::member_type &tea
         double energy_i = 0.0;
         coord_t pos_i = flip_data.lattice_nodes_positions(i);
         double theta_i = flip_data.sequence_on_lattice(pos_i);
-        printf(" i = %d \n", i);
+        printf(" i = %ld \n", i);
         long num_j = flip_data.L - (i + 1);
         Kokkos::parallel_reduce(
                 Kokkos::TeamVectorRange(team_member, num_j),
@@ -628,7 +628,7 @@ void hierarchicalEnergy(const Kokkos::TeamPolicy<Kokkos::Cuda>::member_type &tea
 
                     long j = i + 1 + jj;
 
-                    printf(" j = %d \n", jj);
+                    printf(" j = %ld \n", jj);
                     coord_t pos_j = flip_data.lattice_nodes_positions(j);
                     double theta_j = flip_data.sequence_on_lattice(pos_j);
 
@@ -638,7 +638,7 @@ void hierarchicalEnergy(const Kokkos::TeamPolicy<Kokkos::Cuda>::member_type &tea
                     double contrib = Kokkos::cos(theta_i - theta_j) / r_val;
                     innerSum += contrib;
 
-                    printf(" i = %d j = %d  contrib = %f ; r_val = %f; t1 = %f; t2 = %f \n",
+                    printf(" i = %ld j = %ld  contrib = %f ; r_val = %f; t1 = %f; t2 = %f \n",
                            i, j, contrib, r_val, theta_i, theta_j);
                 },
                 energy_i
