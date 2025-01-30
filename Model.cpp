5fce358fd7d3a616a6f83747f8ce8d6077625518
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-01-30 20:35:31 +0300

    errors in energy

--- Model.cpp
+++ Model.cpp
@@ -705,7 +705,8 @@ void XY_SAW_LongInteraction::FlipMove_AddEnd() {
     int teamSize = 128;
     int numTeams = (L + teamSize - 1) / teamSize;
     int vectorLength = 1;
-    team_policy policy(numTeams, teamSize, vectorLength);
+    team_policy policy(L, 128);
+    //team_policy policy(numTeams, teamSize, vectorLength);
 
     //team_policy policy(1, 128);
 
