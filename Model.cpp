4c413b2980cbaa9bd66f7d11af7031b9bd8ebdad
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-02-03 22:23:40 +0300

    find problems in hE

--- Model.cpp
+++ Model.cpp
@@ -691,9 +691,9 @@ void hierarchicalEnergy(const Kokkos::TeamPolicy<Kokkos::Cuda>::member_type &tea
                 // Subtract the computed energy contribution.
                 //H_total -= energy_i;
 
-                Kokkos::single(Kokkos::PerTeam(team_member), [&]() {
+                //Kokkos::single(Kokkos::PerTeam(team_member), [&]() {
                     H_total -= energy_i;
-                });
+                //});
 
                 // Optionally, print the computed energy for each i (printed only by one thread per team).
                 /*if (team_member.team_rank() == 0) {
