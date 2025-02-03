fcf78433da19f3459cd48a252278e32bf23d04e8
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-02-03 20:31:02 +0300

    errors in energy

--- Model.cpp
+++ Model.cpp
@@ -434,9 +434,19 @@ void XY_SAW_LongInteraction::Energy() {
                 energy_i
         );
         // Each team contributes to the total energy
-        Kokkos::single(Kokkos::PerTeam(team_member), [&]() {
+        /*Kokkos::single(Kokkos::PerTeam(team_member), [&]() {
             H_total -= energy_i;
-        });
+        });*/
+
+
+        H_total -= energy_i;
+        // (Optionally, if you want to print only once per team you might do:)
+        if (team_member.team_rank() == 0) {
+            printf(" i = %ld    e_i = %f \n", i, energy_i);
+        }
+
+
+
     }, flip_data_local.newE );
 }
 
