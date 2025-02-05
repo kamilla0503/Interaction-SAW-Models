beed36ecb5994a1b0ada411a2936b16af0a389b4
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-02-05 22:42:43 +0300

    find problems in hE

--- Model.cpp
+++ Model.cpp
@@ -687,7 +687,7 @@ void hierarchicalEnergy(const Kokkos::TeamPolicy<Kokkos::Cuda>::member_type &tea
                 );
                     //H_total -= energy_i;
 
-                Kokkos::single(Kokkos::PerTeam(team_member), [&]() {
+                Kokkos::single(Kokkos::PerThread(team_member), [&]() {
                     H_total -= energy_i;
                 });
 
