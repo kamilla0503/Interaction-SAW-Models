e2b0a5ca4429a8a64787cfdc183b530168288f48
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-02-13 13:29:54 +0300

    check 1023

--- Model.cpp
+++ Model.cpp
@@ -682,7 +682,7 @@ void XY_SAW_LongInteraction::FlipMove_AddEnd() {
     //int teamSize = 128;
    // int numTeams = (L + teamSize - 1) / teamSize;
     int vectorLength = 1;
-    team_policy policy(1, 512, 1);
+    team_policy policy(1, 1023, 1);
     //team_policy policy(1, 32, 16); //not bad choice
     //team_policy policy(numTeams, teamSize, vectorLength);
 
@@ -814,7 +814,7 @@ void XY_SAW_LongInteraction::FlipMove_AddStart() {
     //int teamSize = 128;
     // int numTeams = (L + teamSize - 1) / teamSize;
     int vectorLength = 1;
-    team_policy policy(1, 512, 1);
+    team_policy policy(1, 1023, 1);
     // team_policy policy(1, 32, 16); //not bad choice
     //team_policy policy(numTeams, teamSize, vectorLength);
 
