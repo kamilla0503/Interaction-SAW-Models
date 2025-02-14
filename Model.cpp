608954e5a1b634934113b1262a660b42123a3e88
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-02-14 19:14:12 +0300

    try more merge

--- Model.cpp
+++ Model.cpp
@@ -884,12 +884,12 @@ void XY_SAW_LongInteraction::LaunchIterations (long long n_iters)  {
                 //printf(" step = %lld \n", step);
                 auto rand_gen = local_pool.get_state();
                 double mc_step_type = rand_gen.drand(0.0, 1.0);
-                pool.free_state(rand_gen);
+                local_pool.free_state(rand_gen);
                 // Decide which type of update to perform:
                 if (mc_step_type < p_for_local_update) {
                     auto rand_gen2 = local_pool.get_state();
                     double flipMoveType = rand_gen2.drand(0.0, 1.0);
-                    pool.free_state(rand_gen2);
+                    local_pool.free_state(rand_gen2);
                     if (flipMoveType < 0.5) {
                         // Instead of FlipMove_AddEnd(), call your hierarchical energy update for "end"
                         hierarchicalOneKernel_AddEnd_FirstPart(team_member, flip_data_local, local_pool);
