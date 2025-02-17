ff1efa8a3ccd9c75247b5359d68b6234726cb4ac
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-02-17 18:20:50 +0300

    try more merge

--- Model.cpp
+++ Model.cpp
@@ -826,19 +826,17 @@ void XY_SAW_LongInteraction::LaunchIterations (long long n_iters)  {
         // Ensure only one thread per team drives the simulation loop.
        // Kokkos::single(Kokkos::PerTeam(team_member), [&]() {
             for (long long step = 0; step < flip_data_local.iters_to_update(); ++step) {
-                Kokkos::single(Kokkos::PerTeam(team_member), [&]() {
+            //    Kokkos::single(Kokkos::PerTeam(team_member), [&]() {
                 printf("step = %lld \n", step);
                 // Generate the random number once.
                 {
                     auto rand_gen2 = local_pool.get_state();
                     flip_data_local.flip_move_type() = rand_gen2.drand(0.0, 1.0);
                     local_pool.free_state(rand_gen2);
-
-
-                    printf("before barrier %f \n",flip_data_local.flip_move_type()  ); 
+                    printf("before barrier %f \n",flip_data_local.flip_move_type()  );
                 }
 
-                });
+              //  });
 
               //  printf("before barrier %f \n",flip_data_local.flip_move_type()  );
 
