8af4408bea01309f38d6399802da9b8ac3f4ec56
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-02-18 21:26:26 +0300

    try more merge

--- Model.cpp
+++ Model.cpp
@@ -741,7 +741,9 @@ void hierarchicalOneKernel_AddStart_FirstPart(const Kokkos::TeamPolicy<Kokkos::C
     bool accept_move = hierarchicalFlipMoveAddStart(team_member, flip_data_local, pool);
     //printf("hierarchicalOneKernel dir  = %ld; end = %ld;   \n ",   flip_data_local.direction(),flip_data_local.end_conformation(0));
 
-    team_member.team_broadcast([&](bool& val) { val = accept_move; }, 0);
+    accept_move = Kokkos::single(Kokkos::PerTeam(team_member), accept_move);
+
+    // team_member.team_broadcast([&](bool& val) { val = accept_move; }, 0);
 
     team_member.team_barrier();
 
