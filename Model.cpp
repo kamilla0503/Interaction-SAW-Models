0abe01c413a7680b1e8912a92adfb81936f0bdd5
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-02-18 20:31:50 +0300

    try more merge

--- Model.cpp
+++ Model.cpp
@@ -859,12 +859,12 @@ void XY_SAW_LongInteraction::FlipMove_AddStart() {
                          KOKKOS_LAMBDA(const member_type &team_member) {
         hierarchicalOneKernel_AddStart_FirstPart(team_member, flip_data_local, local_pool);
         team_member.team_barrier();
-        hierarchicalOneKernel_AddEnd_FirstPart(team_member, flip_data_local, local_pool);
-        team_member.team_barrier();
-        hierarchicalOneKernel_AddStart_FirstPart(team_member, flip_data_local, local_pool);
-        team_member.team_barrier();
-        hierarchicalOneKernel_AddEnd_FirstPart(team_member, flip_data_local, local_pool);
-        team_member.team_barrier();
+//        hierarchicalOneKernel_AddEnd_FirstPart(team_member, flip_data_local, local_pool);
+//        team_member.team_barrier();
+//        hierarchicalOneKernel_AddStart_FirstPart(team_member, flip_data_local, local_pool);
+//        team_member.team_barrier();
+//        hierarchicalOneKernel_AddEnd_FirstPart(team_member, flip_data_local, local_pool);
+//        team_member.team_barrier();
     }
     );
 }
