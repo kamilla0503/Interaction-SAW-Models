9231f65f7c4c50aef9757fb2b3a306483d375d2e
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-02-23 17:01:56 +0300

    try more merge

--- Model.cpp
+++ Model.cpp
@@ -906,7 +906,7 @@ void XY_SAW_LongInteraction::FlipMove_AddEnd() {
     //int teamSize = 128;
     // int numTeams = (L + teamSize - 1) / teamSize;
     int vectorLength = 1;
-    team_policy policy(1, 510, 1);
+    team_policy policy(1, 1020, 1);
     //team_policy policy(1, 32, 16); //not bad choice
     //team_policy policy(numTeams, teamSize, vectorLength);
 
