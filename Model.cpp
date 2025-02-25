4fef0363fcee0122581aed7118e36f4cb076711c
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-02-25 20:10:02 +0300

    try mergy

--- Model.cpp
+++ Model.cpp
@@ -620,13 +620,16 @@ void hierarchicalOneKernel_AddEnd_FirstPart(const Kokkos::TeamPolicy<Kokkos::Cud
     //printf("hierarchicalOneKernel dir  = %ld; end = %ld;   \n ",   flip_data_local.direction(),flip_data_local.end_conformation(0));
    // team_member.team_barrier(); Do I need it?
 
+    hierarchicalEnergy(team_member, flip_data_local);
+    
     if (!accept_move) {
         return;
     }
-    hierarchicalEnergy(team_member, flip_data_local);
+   // hierarchicalEnergy(team_member, flip_data_local);
    // team_member.team_barrier();
 
     Kokkos::single(Kokkos::PerTeam(team_member), [&]() {
+        printf("hierarchicalOneKernel Energy %f \n", flip_data_local.newE());
         //printf("single dir  = %ld; end = %ld;   \n ",   flip_data_local.direction(),flip_data_local.end_conformation(0));
         //printf("hierarchicalOneKernel Energy %f \n", flip_data_local.newE());
         double p1 = exp( -(flip_data_local.J * (flip_data_local.newE() - flip_data_local.E(0))) );
@@ -756,12 +759,15 @@ void hierarchicalOneKernel_AddStart_FirstPart(const Kokkos::TeamPolicy<Kokkos::C
 
     // team_member.team_barrier(); Do I need it?
 
+    hierarchicalEnergy(team_member, flip_data_local);
     if (!accept_move) {
         return;
     }
-    hierarchicalEnergy(team_member, flip_data_local);
+    //hierarchicalEnergy(team_member, flip_data_local);
 
     Kokkos::single(Kokkos::PerTeam(team_member), [&]() {
+
+        printf("hierarchicalOneKernel Add Start Energy %f \n", flip_data_local.newE());
         double p1 = exp(-(flip_data_local.J * (flip_data_local.newE() - flip_data_local.E(0))));
         double p_metropolis = Kokkos::min(1.0, p1);
         auto rand_gen = pool.get_state();
