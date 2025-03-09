b9c617d26e23f7dce98e4d7f32227893468fd7e3
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-03-09 21:40:05 +0300

    check 1023

--- Model.cpp
+++ Model.cpp
@@ -809,7 +809,7 @@ void XY_SAW_LongInteraction::runMCMCOnDevice(long long MC_STEPS=10000)
     auto local_pool = my_pool;
     // (C) We launch exactly one team, with 1023 threads, as you do now
     using team_policy = Kokkos::TeamPolicy<Kokkos::Cuda>;
-    team_policy policy(1, 512, 1);
+    team_policy policy(1, 1023, 1);
     //team_policy policy(1, 1, 512);
     auto n_iters = MC_STEPS;
 
