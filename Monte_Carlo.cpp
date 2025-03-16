97b94c2e4f3d665582eec872063fd67b856fcf41
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-03-16 17:40:13 +0300

    not working all before

--- Monte_Carlo.cpp
+++ Monte_Carlo.cpp
@@ -146,7 +146,7 @@ void MC_Interacting_SAW_XY::run_simulation(double J) {
             model->out_MC_data(MCDataStream, i);
             model->defect(defect_DataStream, i);
             model->out_angle_data(angles_DataStream, i);
-            model->out_dir_data(angles_DataStream, i);
+            model->out_dir_data(dirs_DataStream, i);
         }
     }
 
