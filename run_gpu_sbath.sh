f5538437de9329a2f075c7e197a5589a90abafc3
kamilla0503 <kamilla.faizullina@yandex.ru>
2025-03-19 14:26:55 +0300

    just little data

--- run_gpu_sbath.sh
+++ run_gpu_sbath.sh
@@ -8,13 +8,23 @@ resultFolder=data/XY_MC/GPU_Results/R_3/long_view_short
 
 #rm slurm*.out
 
-for L in 200
+
+for L in 300
 do
-  for J in 0.2 0.22 0.24 0.26 0.28 0.3
+  for J in 0.22 0.24 0.26 0.28 0.3
   do
-  sbatch --time=2-30:0 --gpus=1 --wrap="./finite_element.cuda $L $J $resultFolder"
+  sbatch --time=0-5:0 --gpus=1 --wrap="./finite_element.cuda $L $J $resultFolder"
   done
 done
+
+
+# for L in 200
+# do
+#   for J in 0.31 0.305
+#   do
+#   sbatch --time=0-2:0 --gpus=1 --wrap="./finite_element.cuda $L $J $resultFolder"
+#   done
+# done
 
 # for L in 400
 # do
